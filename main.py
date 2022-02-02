90ea6c7f4702b15614fa288a0d065c2d501d235c
rushil <rushiling121@gmail.com>
2022-02-02 14:29:48 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,98 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
+from flask_ngrok import run_with_ngrok
+
+app = Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['DEBUG'] = True
+run_with_ngrok(app)
+db = SQLAlchemy(app)
+
+
+def abort_on_presence(presence_id):
+    user = UserModel.query.filter_by(id=presence_id).first()
+    if not user:
+        abort(404, message="User not found")
+
+class UserModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    Email = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return f"User(name = {name}, Email = {Email})"
+
+user_put_args = reqparse.RequestParser()
+user_put_args.add_argument("name", type=str, help="Name of the user is required", required=True)
+user_put_args.add_argument("Email", type=str, help="Views of the user", required=True)
+
+user_update_args = reqparse.RequestParser()
+user_update_args.add_argument("name", type=str, help="Name of the user is required")
+user_update_args.add_argument("Email", type=str, help="Views of the user")
+
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'Email': fields.String
+}
+
+
+db.create_all()
+
+
+class User(Resource):
+    @marshal_with(resource_fields)
+    def get(self, user_id):
+        result = UserModel.query.filter_by(id=user_id).first()
+        if not result:
+            abort(404, message="Could not find user with that id")
+        return result 
+
+    @marshal_with(resource_fields)
+    def put(self, user_id):
+        args = user_put_args.parse_args()
+        result = UserModel.query.filter_by(id=user_id).first()
+        if result:
+            abort(409, message="User id taken...")
+
+
+        user = UserModel(id=user_id, name=args['name'], Email=args['Email'])
+        db.session.add(user)
+        db.session.commit()
+        return user, 201
+
+    @marshal_with(resource_fields)
+    def patch(self, user_id):
+        args = user_update_args.parse_args()
+        result = UserModel.query.filter_by(id=user_id).first()
+        if not result:
+            abort(404, message="User doesn't exist, cannot update")
+
+        if args['name']:
+            result.name = args['name']
+        if args['Email']:
+            result.views = args['Email']
+
+        db.session.commit()
+
+        return result
+
+
+    def delete(self, user_id):
+        abort_on_presence(user_id)
+        UserModel.query.filter_by(id=user_id).delete()
+        db.session.commit()
+        return '', 204
+    
+class AllUsers(Resource):
+    @marshal_with(resource_fields)
+    def get(self):
+        return UserModel.query.all()
+
+api.add_resource(User, "/user/<int:user_id>")
+api.add_resource(AllUsers,"/alluser/")
+
+if __name__ == "__main__":
+    app.run()
